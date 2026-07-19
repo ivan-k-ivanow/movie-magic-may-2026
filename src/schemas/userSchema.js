575312a0d0f395fe77e1e6481761c2c3deeb0763
ivan-k-ivanow <i.ivanow.k@gmail.com>
2026-07-19 16:40:55 +0300

    remove artist id from movies

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,19 +1,31 @@
 import * as z from "zod";
+import bcrypt from 'bcrypt';
+
 import userRepository from "../repositories/userRepository.js";
+
 
 export const createUserSchema = z.object({
     email: z.string()
         .email({ message: "Invalid email address" })
-        .min(10)
+        .min(10, { message: "Email must be at least 10 characters long"})
+        .trim()
         .refine(async (value) => {
-           const result = await userRepository.findByEmail(value);
-           return !result;
-        }, { error: "Email already exists"}),
+            const result = await userRepository.findByEmail(value);
+
+            return !result;
+        }, { error: "Email already exists" }),
     password: z.string()
         .min(6)
         .regex(/^[a-zA-Z0-9]+$/, { message: "Password must contain at least one letter or number" }),
+
     repeatPassword: z.string(),
 }).refine((data) => data.password === data.repeatPassword, {
     message: "Passwords do not match",
     path: ["repeatPassword"],
-}).transform(({ repeatPassword, ...data }) => data)
+}).transform(({ repeatPassword, ...data }) => {
+    const hashedPassword = bcrypt.hashSync(data.password, 10);
+    return {
+        ...data,
+        password: hashedPassword,
+    };
+});
